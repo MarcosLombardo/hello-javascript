bf7387234a3cbfc0db515fa2a36101ca6d95c4a6
MarcosLombardo <marcoslombardo28@gmail.com>
2024-08-29 21:55:27 -0300

    Ejercicios 27 y 28 resueltos

--- Basic/27-console-methods-exercises.js
+++ Basic/27-console-methods-exercises.js
@@ -6,20 +6,92 @@ https://www.youtube.com/live/PAnxhBE5kIE?si=V0F_NsKO9lmhhatu&t=555
 
 // 1. Crea un función que utilice error correctamente
 
+function errorLog() {
+  console.error("Este es un mensaje de error");
+}
+
+errorLog();
+
 // 2. Crea una función que utilice warn correctamente
+
+function warnLog() {
+  console.warn("Este es un mensaje de advertencia");
+}
+
+warnLog();
 
 // 3. Crea una función que utilice info correctamente
 
+function infoLog() {
+  console.info("Este es un mensaje de información");
+}
+
+infoLog();
+
 // 4. Utiliza table
+
+function tableLog() {
+  const users = [
+    { name: "Carlos", age: 43 },
+    { name: "Juan", age: 40 },
+  ];
+  console.table(users);
+}
+
+tableLog();
 
 // 5. Utiliza group
 
+function groupLog() {
+  console.group("Usuario:");
+  console.log("Nombre: Marcos");
+  console.log("Edad: 33");
+  console.groupEnd();
+}
+
+groupLog();
+
 // 6. Utiliza time
+
+function timeLog() {
+  console.time("Tiempo de ejecución");
+  for (let i = 0; i < 1000000; i++) {}
+  console.timeEnd("Tiempo de ejecución");
+}
+
+timeLog();
 
 // 7. Valida con assert si un número es positivo
 
+let number;
+function validateLog(number) {
+  console.assert(number > 0, "El número no es positivo");
+}
+
+validateLog(10);
+
 // 8. Utiliza count
+
+function countLog() {
+  console.count("Conteo");
+  console.count("Conteo");
+  console.count("Conteo");
+}
+
+countLog();
 
 // 9. Utiliza trace
 
+function traceLog() {
+  console.trace("Registro de la ejecución");
+}
+
+traceLog();
+
 // 10. Utiliza clear
+
+function clearLog() {
+  console.clear();
+}
+
+clearLog();
--- Basic/31-modules-exercises.js
+++ Basic/31-modules-exercises.js
@@ -4,22 +4,47 @@ Console y módulos
 https://www.youtube.com/live/PAnxhBE5kIE?si=V0F_NsKO9lmhhatu&t=555
 */
 
-// 1. Exporta una función
+// // 1. Exporta una función
+// export function myFunction() {
+//   console.log("Esta es una función exportada");
+// }
 
-// 2. Exporta una constante
+// // 2. Exporta una constante
+// export const MY_CONSTANT = 42;
 
-// 3. Exporta una clase
+// // 3. Exporta una clase
+// export class MyClass {
+//   constructor() {
+//     console.log("Clase exportada");
+//   }
+// }
 
-// 4. Importa una función
+// // 4. Importa una función
+// import { myFunction } from './myModule';
 
-// 5. Importa una constante
+// // 5. Importa una constante
+// import { MY_CONSTANT } from './myModule';
 
-// 6. Importa una clase
+// // 6. Importa una clase
+// import { MyClass } from './myModule';
 
-// 7. Exporta una función, una constante y una clase por defecto (en caso de que lo permita)
+// // 7. Exporta una función, una constante y una clase por defecto
+// export default {
+//   myFunction,
+//   MY_CONSTANT,
+//   MyClass,
+// };
 
-// 8. Importa una función, una constante y una clase por defecto (en caso de que lo permita)
+// // 8. Importa una función, una constante y una clase por defecto
+// import myModule from './myModule';
+// const { myFunction, MY_CONSTANT, MyClass } = myModule;
 
-// 9. Exporta una función, una constante y una clase desde una carpeta
+// // 9. Exporta una función, una constante y una clase desde una carpeta
+// export { myFunction } from './folder/myFunction';
+// export { MY_CONSTANT } from './folder/myConstant';
+// export { MyClass } from './folder/MyClass';
 
-// 10. Importa una función, una constante y una clase desde un directorio diferente al anterior
+// // 10. Importa una función, una constante y una clase desde un directorio diferente al anterior
+// import { myFunction } from '../anotherFolder/myFunction';
+// import { MY_CONSTANT } from '../anotherFolder/myConstant';
+// import { MyClass } from '../anotherFolder/MyClass';
